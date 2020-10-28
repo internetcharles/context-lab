215f1c011a5cfe4caf428a63c9b251651d5d4128
internetcharles <charlesodellsmith@gmail.com>
2020-10-28 15:40:32 -0700

    tests pass?

--- src/components/app/App.test.jsx
+++ src/components/app/App.test.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render, cleanup } from '@testing-library/react';
 import App from './App';
+import { ThemePickerProvider } from '../../context/ThemePickerContext';
 
 describe('App component', () => {
   afterEach(() => cleanup());
   it('renders App', () => {
-    const { asFragment } = render(<App />);
+    const { asFragment } = render(
+      <ThemePickerProvider>
+        <App />
+      </ThemePickerProvider>);
     expect(asFragment()).toMatchSnapshot();
   });
 });
--- src/services/hey-arnold-api.js
+++ src/services/hey-arnold-api.js
@@ -1,4 +1,6 @@
 
+import fetch from 'node-fetch';
+
 export const getCharacters = (page) => {
   // eslint-disable-next-line max-len
   return fetch(`https://hey-arnold-api.herokuapp.com/api/v1/characters?perPage=20&page=${page}`)
